6776be497e46c39b96a7880ba4958aad089e5487
Christian Albrecht <christian@coastal-consulting.de>
2019-10-29 12:57:31 +0100

    sign in email-password

--- lib/auth.ex
+++ lib/auth.ex
@@ -92,7 +92,7 @@ defmodule FirebaseAdminEx.Auth do
 	  do_request("accounts:resetPassword?apiKey="<>api_key, code, client_email, project_id)
   end
   
-  def sign_in_with_email_and_password(credentials, client_email) do
+  def sign_in_with_email_and_password(credentials, client_email \\ nil) do
 	  do_request("accounts:signInWithPassword", credentials, client_email)
   end
   
